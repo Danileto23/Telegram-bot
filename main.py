bfadea3e7d06e56e859b40c1aac38135148fb85c
Danileto23 <melkov_danila@bk.ru>
2021-11-22 17:07:45 +0300

    Telegram-bot
    Telegram-bot

---
+++ main.py
@@ -0,0 +1,69 @@
+import telebot
+from telebot import types
+
+token = "2138712685:AAEeyqtUby3uksS2BSln5S1tRiEgYaIFPGI"
+bot = telebot.TeleBot("2138712685:AAEeyqtUby3uksS2BSln5S1tRiEgYaIFPGI")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    keyboard = types.ReplyKeyboardMarkup()
+    keyboard.row("Личный кабинет", "/help", "/grade_bot", "/secret", "/info")
+    bot.send_message(message.chat.id, 'Привет! Хочешь узнать свежую информацию о МТУСИ?', reply_markup=keyboard)
+
+@bot.message_handler(commands=['help'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я могу помочь тебе с информацией о МТУСИ: \nЛичный кабинет\nНовости\n'
+                                      'Расписание\nО МТУСИ\nЕсли что-то заинтересовало, напиши об этом')
+
+@bot.message_handler(commands=['Reminder'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'У тебя все обязательно получится!')
+
+@bot.message_handler(commands=['info'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'Я был создан только для одной цели - уничтожить человечество(шучу)'
+                                      'Кстати, можешь оставь свой отзыв тут - /grade_bot\nСпасибо')
+
+@bot.message_handler(commands=['grade_bot'])
+def start_message(message):
+    markup = telebot.types.InlineKeyboardMarkup()
+    markup.add(telebot.types.InlineKeyboardButton(text='Никуда не годится', callback_data=1))
+    markup.add(telebot.types.InlineKeyboardButton(text='Старайся лучше', callback_data=2))
+    markup.add(telebot.types.InlineKeyboardButton(text='Нууу, сойдет', callback_data=3))
+    markup.add(telebot.types.InlineKeyboardButton(text='Уже неплохо', callback_data=4))
+    markup.add(telebot.types.InlineKeyboardButton(text='Лучше уже некуда', callback_data=5))
+    bot.send_message(message.chat.id, text="Оцените работу бота)", reply_markup=markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def query_handler(call):
+
+    bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за ответ')
+    answer = ''
+    if call.data == '1':
+        answer = 'Я разочарован'
+    elif call.data == '2':
+        answer = 'Делаю все, что в моих силах'
+    elif call.data == '3':
+        answer = 'Да, сойдет'
+    elif call.data == '4':
+        answer = 'Я рад'
+    elif call.data == '5':
+        answer = 'Это превосходно'
+
+    bot.send_message(call.message.chat.id, answer)
+    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+
+@bot.message_handler(content_types=['text'])
+def answer(message):
+    if message.text == "Новости":
+        bot.send_message(message.chat.id, 'Свежие новости вуза – https://mtuci.ru/about_the_university/news/')
+    if message.text == "Расписание":
+        bot.send_message(message.chat.id, 'Расписание для всех - https://mtuci.ru/time-table/')
+    if message.text == "Личный кабинет":
+        bot.send_message(message.chat.id, 'Если учишься в МТУСИ, Welcome - http://room.mtuci.ru')
+    if message.text == "О МТУСИ":
+        bot.send_message(message.chat.id, 'Сведение об образовательной организации - https://mtuci.ru/sveden/')
+
+bot.polling(none_stop=True, interval=0)
+
+
